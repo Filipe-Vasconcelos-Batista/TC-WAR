5badc7a65ad416c01989055d14000e2d74b9193c
Filipe Batista <124799878+papadok2950@users.noreply.github.com>
2023-08-07 14:53:00 +0200

    Add files via upload

---
+++ war.py
@@ -0,0 +1,30 @@
+#creating the card game war in python  
+
+#1- create a object class for the deck #deck has 52 cards 
+#deck is randomily shuffled at the beggining of every game 
+#each player gets half of the cads in the beggining (26) 
+
+
+#2- create a object class for the cards (can be just one with various definitions) 
+#cards are ,4A ,4K ,4Q ,4J ,410 ,49 ,48 ,47 ,46 ,45 ,44 ,43, 4*2
+ #in the battle the bigger the number allways wins 
+
+
+#3-make the random distribution of the 52 cards
+#26 cards for each polayer
+#maybe a list of random cards #this list as to keep the same order through the game
+#the player cannot see his cards 4- make the game run without the war element 
+#every turn two cards are played #the player with the bigger card wins
+
+
+#5-make the war element of the game 
+#1 card down and one turned up 
+#compete to see whos the highest #if they are equal restart 
+
+
+#6- create the winning condition(in this case the total absortion of the deck by one player?)
+ #???? 
+
+
+#7- give out a victory message
+#You are victorious
